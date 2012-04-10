f17cc9ff57f779bd633181dcb1daada19fc94ec2
Jan Vlnas <git@jan.vlnas.cz>
2012-04-10 22:55:55 +0200

    Fix config/database.yml copying
    Copypasta creeped in

--- test/ci/before_script.sh
+++ test/ci/before_script.sh
@@ -16,8 +16,8 @@ TARGET_DIR=$HOME/chiliproject
 
 # Prepare ChiliProject
 git clone --depth=100 $MAIN_REPO $TARGET_DIR
-
 cd $TARGET_DIR
+#git submodule update --init --recursive
 
 # Copy over the already downloaded plugin 
 cp -r ~/builds/*/$REPO_NAME vendor/plugins/$PLUGIN_DIR
@@ -30,10 +30,10 @@ echo "creating $DB database"
 case $DB in
   "mysql" )
     mysql -e 'create database chiliproject_test;'
-    cp spec/ci/database.mysql.yml config/database.yml;;
+    cp config/database.mysql.yml config/database.yml;;
   "postgres" )
     psql -c 'create database chiliproject_test;' -U postgres
-    cp spec/ci/database.postgres.yml config/database.yml;;
+    cp config/database.postgres.yml config/database.yml;;
 esac
 
 bundle exec rake db:migrate
