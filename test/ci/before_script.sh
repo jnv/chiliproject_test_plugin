f78d5bfd78abbe6156c8ac140461183c3ae00b05
Jan Vlnas <git@jan.vlnas.cz>
2012-04-10 23:51:57 +0200

    Move configuration to env vars in .travis.yml

--- test/ci/before_script.sh
+++ test/ci/before_script.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
 # Git repo of the ChiliProject/Redmine
-#MAIN_REPO="git://github.com/chiliproject/chiliproject.git"
-MAIN_REPO="git://github.com/jnv/chiliproject-fit.git"
-
-# Name of your plugin's repo
-REPO_NAME=chiliproject_test_plugin
-
-# Plugin's name/directory
-# usually will be same as $REPO_NAME
-PLUGIN_NAME=chiliproject_test_plugin
-
-# Where the cloned ChiliProject will be stored
-TARGET_DIR=$HOME/chiliproject
 
 # Prepare ChiliProject
 git clone --depth=100 $MAIN_REPO $TARGET_DIR
@@ -22,9 +10,9 @@ cd $TARGET_DIR
 # Copy over the already downloaded plugin 
 cp -r ~/builds/*/$REPO_NAME vendor/plugins/$PLUGIN_DIR
 
-export BUNDLE_GEMFILE=$TARGET_DIR/Gemfile
+#export BUNDLE_GEMFILE=$TARGET_DIR/Gemfile
 
-bundle install --without development sqlite #FIXME: support for bundler_args
+bundle install $BUNDLER_ARGS
 
 echo "creating $DB database"
 case $DB in
--- test/ci/script.sh
+++ test/ci/script.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-TARGET_DIR=$HOME/chiliproject
-
-PLUGIN_NAME=chiliproject_test_plugin
-
 cd $TARGET_DIR
-pwd
-echo "BUNDLE_GEMFILE:" $BUNDLE_GEMFILE
-bundle list
 bundle exec rake test:engines:all PLUGIN=$PLUGIN_NAME
